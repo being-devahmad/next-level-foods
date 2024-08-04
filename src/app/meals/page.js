b5ba1d256bf43859832fc9e6a10142d41323203e
being-devahmad <devahmad41@gmail.com>
2024-08-04 13:28:38 +0500

    meals component commit

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,9 +1,19 @@
+import classes from "./page.module.css"
+
+
 export default function MealsPage() {
     return (
         <>
-            <div className="flex items-center justify-center text-6xl">
-                <h1>This is Meals Page</h1>
-            </div>
+            <header className={classes.header}>
+                <h1>Delicious meals , created <span className={classes.highlight}>by you</span></h1>
+                <p>Choose your favourite recipe and cook it yourself</p>
+                <p className={classes.cta}>
+                    <Link href={'/meals/share'}>
+                        Share your favorite recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}></main>
         </>
     )
 }
