076a23ff4017879508f333dd5beeb0557cb5e854
being-devahmad <devahmad41@gmail.com>
2024-08-05 01:02:36 +0500

    image-picker preview added
     // This will make mime or blob url for the picked or selected image
            const fileReader = new FileReader()
            fileReader.onload = () => {
                setPickedImage(fileReader.result)
            }
            fileReader.readAsDataURL(file)

--- src/components/meals/image-picker.js
+++ src/components/meals/image-picker.js
@@ -1,20 +1,42 @@
 'use client'
 
-import { useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 import classes from "./image-picker.module.css"
+import Image from "next/image"
 
 export default function ImagePicker({ label, name }) {
-
     const imageInput = useRef()
-
     function handlePicClick() {
         imageInput.current.click()
     }
+
+    const [pickdImage, setPickedImage] = useState([])
+    const handleImageChange = (event) => {
+        const file = event.target.files[0]
+        if (!file) {
+            return
+        }
+
+        // This will make mime or blob url for the picked or selected image
+        const fileReader = new FileReader()
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result)
+        }
+        fileReader.readAsDataURL(file)
+    }
+
+    useEffect(() => {
+        console.log('pickedImage-->', pickdImage)
+    }, [pickdImage])
 
     return (
         <div className={classes.picker}>
             <label htmlFor="image">{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickdImage && <p>No image picked yet.</p>}
+                    {pickdImage && <Image src={pickdImage} fill />}
+                </div>
                 <input
                     ref={imageInput}
                     className={classes.input}
@@ -22,6 +44,7 @@ export default function ImagePicker({ label, name }) {
                     id="image"
                     accept="image/png , image/jpg"
                     name={name}
+                    onChange={handleImageChange}
                 />
                 <button
                     className={classes.button}
