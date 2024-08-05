3d4f886b234df14febb46e7d0dc8e2daf06a5d16
ahmadowais-dev <ahmad.owais@intravar.com>
2024-08-05 10:57:58 +0500

    shareMeal , server actions created

--- src/app/meals/share/page.js
+++ src/app/meals/share/page.js
@@ -1,5 +1,6 @@
 import ImagePicker from '@/src/components/meals/image-picker';
 import classes from './page.module.css';
+import {shareMeal} from "@/src/lib/actions";
 
 export default function ShareMealPage() {
     return (
@@ -11,24 +12,24 @@ export default function ShareMealPage() {
                 <p>Or any other meal you feel needs sharing!</p>
             </header>
             <main className={classes.main}>
-                <form className={classes.form}>
+                <form className={classes.form} action={shareMeal}>
                     <div className={classes.row}>
                         <p>
                             <label htmlFor="name">Your name</label>
-                            <input type="text" id="name" name="name" required />
+                            <input type="text" id="name" name="name" required/>
                         </p>
                         <p>
                             <label htmlFor="email">Your email</label>
-                            <input type="email" id="email" name="email" required />
+                            <input type="email" id="email" name="email" required/>
                         </p>
                     </div>
                     <p>
                         <label htmlFor="title">Title</label>
-                        <input type="text" id="title" name="title" required />
+                        <input type="text" id="title" name="title" required/>
                     </p>
                     <p>
                         <label htmlFor="summary">Short Summary</label>
-                        <input type="text" id="summary" name="summary" required />
+                        <input type="text" id="summary" name="summary" required/>
                     </p>
                     <p>
                         <label htmlFor="instructions">Instructions</label>
@@ -39,7 +40,7 @@ export default function ShareMealPage() {
                             required
                         ></textarea>
                     </p>
-                    <ImagePicker />
+                    <ImagePicker label={'your image'} name={'image'}/>
                     <p className={classes.actions}>
                         <button type="submit">Share Meal</button>
                     </p>
